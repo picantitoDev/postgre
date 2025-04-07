2b725759b0521180455e5d71a32bf3fdda558bb4
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-04-07 15:55:53 -0500

    installed dotenv

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg")
+require("dotenv").config()
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "pieroarturo",
-  database: "top_users",
-  password: "picantito12",
-  port: 5432, // The default port
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 })
