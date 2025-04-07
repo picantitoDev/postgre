d4a2209527d851b64096a354fce01033043fcccd
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-04-07 18:39:36 -0500

    Switched to internal conecction

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ const { Pool } = require("pg")
 require("dotenv").config()
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
 })
